c8ede1a10b1950387e55fd6ea06dbbb4121d0ffc
suli <suli@szaisiou.com>
2019-05-31 11:36:56 +0800

    myfirst commit

---
+++ src/com/su/test/Test.java
@@ -0,0 +1,19 @@
+package com.su.test;
+
+/**
+ * @Description
+ * @Author huangjianye
+ * @Date $date$ $time$
+ * @Param $param$
+ * @Return $return$
+ * @Exception $exception$
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello ");
+        String hello = new String("hello");
+        System.out.println(hello);
+    }
+}
